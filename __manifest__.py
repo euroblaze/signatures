51d02ddeb4273713c6a2ca97b49c67b93549acd7
VTV24710 <trungvyvo123@gmail.com>
2024-05-16 01:26:01 +0700

    Upgrade Signatures module to V16

--- __manifest__.py
+++ __manifest__.py
@@ -1,34 +1,38 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Signatures",
+    'author': '(initially) Ashant Chalasani, Alieh Vo',
+    'company': 'PowerOn | Wapsol GmbH',
+    'application': True,
+    'category': 'Signatures',
+    'version': '16.0.0.1',
     'summary': """
         Module for multi-company setups which allows for all mails to be branded from the currently active company's domain.!""",
     'description': """
 Signatures
 ==========
-Develop an enterprise 10, 11 compatible module for multi-company setups which allows for all mails to be branded from the currently active company's domain.
+Develop an enterprise 16 compatible module for multi-company setups which allows for all mails to be branded from the currently active company's domain.
 
 Features:
 
 - User can setup specific Signature per company, by going to Signatures which is in the main menu and then choose Add Signature by Company.
 - There the user can see all the signatures added so far, as well as add new signatures for themselves for each company available by clicking Create button.
 - It won't let you create more than one signature for the same company. Each user can create signatures only for themselves, meaning the current active user.
-- Test by going to Preferences (in the main odoo menu) and by choosing the Company -> the field Signature will get the value of the Signature that the selected company has for the currently active user.
+- Test by going to Profile (in the main odoo menu) and by choosing the Preference -> the field Signature will get the value of the Signature that the selected company has for the currently active user.
 - By default the signature field has the value of the currently selected company.
 - Everyone has permissions to access this menu (any type of user).
 - By selecting company the field signature will automatically change its value according to the table where you have previously entered the signature in 'Signatures by Company' under main menu 'Signatures'.
 - Don't forget to click Save when you change the company (and the signature along with it). Signatures field is readonly and can be edited only from the main menu 'Signatures'.
 - Effects all Chatter communications.
     """,
-    'author': 'Simplify-ERP®',
-    'images': 'static/description/icon.png',
-    'application': True,
-    'category': 'Signatures',
-    'version': '0.1',
     'data': [
         'views/config.xml',
-        'views/pref.xml',
+        # 'views/pref.xml',
         'security/ir.model.access.csv'
     ],
     'depends': ['base','mail'],
+    'images': 'static/description/icon.png',
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3'
 }
--- models/models.py
+++ models/models.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, tools
+from odoo import models, fields, api, tools, _
 from odoo.exceptions import UserError, ValidationError
 from email.utils import formataddr
 import base64
+from odoo.tools import is_html_empty, partition, collections, frozendict, lazy_property
+from markupsafe import Markup
+
 
 class Signatures(models.Model):
     _name = 'nebiz.signatures'
@@ -11,15 +14,19 @@ class Signatures(models.Model):
     user_id = fields.Many2one('res.users', 'User', store=True)
     company_id = fields.Many2one('res.company', 'Company', store=True)
     signature = fields.Html(string='Signature', store=True)
-    user_name=fields.Char(string='Name',store=True)
-    user_mail=fields.Char(string='Email',store=True)
-    name = fields.Char(compute='get_name')
+    user_name = fields.Char(string='Name', store=True)
+    user_mail = fields.Char(string='Email', store=True)
+    name = fields.Char(compute='_compute_get_name')
     x_signatures = fields.Char('Firstname')
-    active_signature=fields.Boolean(string='Active Signature',store=True,default=False)
+    active_signature = fields.Boolean(string='Active Signature', store=True, default=False)
 
-    @api.one
-    def get_name(self):
-        self.name = self.user_name
+    @api.depends('user_name')
+    def _compute_get_name(self):
+        for rec in self:
+            name = ''
+            if rec.user_name:
+                name = rec.user_name
+            self.name = name
 
     @api.model
     def create(self, vals):
@@ -29,79 +36,86 @@ class Signatures(models.Model):
 
     # search edit possible only for your own records odoo and write documentation with images
 
-    @api.multi
     def unlink(self):
-        for id in self:
-            if id.user_id.id != self._uid:
+        for rec in self:
+            if rec.user_id.id != self._uid:
                 raise UserError('You can delete only your own signatures!')
-        return super(Signatures,self).unlink()
+        return super(Signatures, self).unlink()
 
-    @api.multi
     def set_signature_active(self):
-        signatures=self.search([('company_id','=',self.company_id.id),('user_id','=',self.user_id.id)])
-        for signature in signatures:
-            signature.active_signature=False
-        self.active_signature=True
+        signatures = self.search([('company_id', '=', self.company_id.id), ('user_id', '=', self.user_id.id)])
+        if signatures:
+            for signature in signatures:
+                signature.active_signature = False
+        self.active_signature = True
 
-    @api.multi
     def disable_signature(self):
-        signatures=self.search([('company_id','=',self.company_id.id),('user_id','=',self.user_id.id)])
-        for signature in signatures:
-            signature.active_signature=False
+        signatures = self.search([('company_id', '=', self.company_id.id), ('user_id', '=', self.user_id.id)])
+        if signatures:
+            for signature in signatures:
+                signature.active_signature = False
 
 
 class Preferences(models.Model):
     _inherit = 'res.users'
 
-    signature = fields.Html(compute='get_signature')
+    # signature = fields.Html(compute='_compute_signature')
 
-    @api.one
-    def get_signature(self):
-        rec = self.env['nebiz.signatures'].search(
-            [('company_id', '=', self.company_id.id), ('user_id', '=', self._uid),('active_signature','=',True)])
-        self.signature = rec.signature
+    def _compute_signature(self):
+        signature_id = self.env['nebiz.signatures'].search(
+            [('company_id', '=', self.company_id.id), ('user_id', '=', self._uid), ('active_signature', '=', True)])
+        if signature_id:
+            for rec in self:
+                rec.signature = signature_id.signature
+        else:
+            for user in self.filtered(lambda user: user.name and is_html_empty(user.signature)):
+                user.signature = Markup('<p>--<br />%s</p>') % user['name']
 
     @api.onchange('company_id')
     def on_change_state(self):
-        rec = self.env['nebiz.signatures'].search(
-            [('company_id', '=', self.company_id.id), ('user_id', '=', self._uid),('active_signature','=',True)])
-        if rec:
-            self.signature = rec.signature
+        signature_id = self.env['nebiz.signatures'].search(
+            [('company_id', '=', self.company_id.id), ('user_id', '=', self._uid), ('active_signature', '=', True)])
+        if signature_id:
+            self.signature = signature_id.signature
         else:
             self.signature = ''
 
     def get_name(self):
-        signature = self.env['nebiz.signatures'].search([('user_id','=',self.id),('company_id','=',self.company_id.id),('active_signature','=',True)])
-        if signature.user_name == False:
-            name=self.display_name
+        signature_id = self.env['nebiz.signatures'].search(
+            [('user_id', '=', self.id), ('company_id', '=', self.company_id.id), ('active_signature', '=', True)])
+        if not signature_id.user_name:
+            name = self.display_name
         else:
-            name=signature.user_name
+            name = signature_id.user_name
         return name
 
     def get_email(self):
-        signature = self.env['nebiz.signatures'].search([('user_id','=',self.id),('company_id','=',self.company_id.id),('active_signature','=',True)])
-        if signature.user_name == False:
-            email=self.email
+        signature_id = self.env['nebiz.signatures'].search(
+            [('user_id', '=', self.id), ('company_id', '=', self.company_id.id), ('active_signature', '=', True)])
+        if not signature_id.user_mail:
+            email = self.email
         else:
-            email=signature.user_mail
+            email = signature_id.user_mail
         return email
+
 
 class ChangeSenderSignature(models.Model):
     _inherit = 'mail.message'
+
     @api.model
     def _get_default_from(self):
         if self.env.user.get_email():
             return formataddr((self.env.user.get_name(), self.env.user.get_email()))
         raise UserError(_("Unable to send email, please configure the sender's email address."))
 
+
 class MailTemplateSignature(models.Model):
     _inherit = 'mail.template'
 
-    def generate_email(self, res_ids, fields=None):
+    def generate_email(self, res_ids, fields):
         """Generates an email from the template for given the given model based on
         records given by res_ids.
 
-        :param template_id: id of the template to render.
         :param res_id: id of the record to use for rendering the template (model
                        is taken from template definition)
         :returns: a dict containing all relevant fields for creating a new
@@ -110,31 +124,18 @@ class MailTemplateSignature(models.Model):
         """
         self.ensure_one()
         multi_mode = True
-        if isinstance(res_ids, (int, long)):
+        if isinstance(res_ids, int):
             res_ids = [res_ids]
             multi_mode = False
-        if fields is None:
-            fields = ['subject', 'body_html', 'email_from', 'email_to', 'partner_to', 'email_cc', 'reply_to', 'scheduled_date']
-
-        res_ids_to_templates = self.get_email_template(res_ids)
-
-        # templates: res_id -> template; template -> res_ids
-        templates_to_res_ids = {}
-        for res_id, template in res_ids_to_templates.iteritems():
-            templates_to_res_ids.setdefault(template, []).append(res_id)
 
         results = dict()
-        for template, template_res_ids in templates_to_res_ids.iteritems():
-            Template = self.env['mail.template']
-            # generate fields value for all res_ids linked to the current template
-            if template.lang:
-                Template = Template.with_context(lang=template._context.get('lang'))
+        for lang, (template, template_res_ids) in self._classify_per_lang(res_ids).items():
             for field in fields:
-                Template = Template.with_context(safe=field in {'subject'})
-                generated_field_values = Template.render_template(
-                    getattr(template, field), template.model, template_res_ids,
-                    post_process=(field == 'body_html'))
-                for res_id, field_value in generated_field_values.iteritems():
+                generated_field_values = template._render_field(
+                    field, template_res_ids,
+                    post_process=(field == 'body_html')
+                )
+                for res_id, field_value in generated_field_values.items():
                     results.setdefault(res_id, dict())[field] = field_value
             # compute recipients
             if any(field in fields for field in ['email_to', 'partner_to', 'email_cc']):
@@ -142,13 +143,19 @@ class MailTemplateSignature(models.Model):
             # update values for all res_ids
             for res_id in template_res_ids:
                 values = results[res_id]
-                # body: add user signature, sanitize
-                if 'body_html' in fields and template.user_signature:
+                if 'body_html' in fields:
                     signature = self.env.user.signature
                     if signature:
-                        values['body_html'] = tools.append_content_to_html(values['body_html'], signature, plaintext=False)
+                        values['body_html'] = tools.append_content_to_html(values['body_html'], signature,
+                                                                           plaintext=False)
                 if values.get('body_html'):
                     values['body'] = tools.html_sanitize(values['body_html'])
+                # if asked in fields to return, parse generated date into tz agnostic UTC as expected by ORM
+                scheduled_date = values.pop('scheduled_date', None)
+                if 'scheduled_date' in fields and scheduled_date:
+                    parsed_datetime = self.env['mail.mail']._parse_scheduled_datetime(scheduled_date)
+                    values['scheduled_date'] = parsed_datetime.replace(tzinfo=None) if parsed_datetime else False
+
                 # technical settings
                 values.update(
                     mail_server_id=template.mail_server_id.id or False,
@@ -162,29 +169,34 @@ class MailTemplateSignature(models.Model):
             if template.report_template:
                 for res_id in template_res_ids:
                     attachments = []
-                    report_name = self.render_template(template.report_name, template.model, res_id)
+                    report_name = template._render_field('report_name', [res_id])[res_id]
                     report = template.report_template
                     report_service = report.report_name
 
                     if report.report_type in ['qweb-html', 'qweb-pdf']:
-                        result, format = Template.env['report'].get_pdf([res_id], report_service), 'pdf'
+                        result, report_format = self.env['ir.actions.report']._render_qweb_pdf(report, [res_id])
                     else:
-                        result, format = odoo_report.render_report(self._cr, self._uid, [res_id], report_service, {'model': template.model}, Template._context)
+                        res = self.env['ir.actions.report']._render(report, [res_id])
+                        if not res:
+                            raise UserError(_('Unsupported report type %s found.', report.report_type))
+                        result, report_format = res
 
                     # TODO in trunk, change return format to binary to match message_post expected format
                     result = base64.b64encode(result)
                     if not report_name:
                         report_name = 'report.' + report_service
-                    ext = "." + format
+                    ext = "." + report_format
                     if not report_name.endswith(ext):
                         report_name += ext
                     attachments.append((report_name, result))
                     results[res_id]['attachments'] = attachments
                     if not results[res_id]['email_from']:
                         if self.env.user.get_email():
-                            results[res_id]['email_from'] = formataddr((self.env.user.get_name(), self.env.user.get_email()))
+                            results[res_id]['email_from'] = formataddr(
+                                (self.env.user.get_name(), self.env.user.get_email()))
 
         return multi_mode and results or results[res_ids[0]]
+
 
 class MailWizzardSignature(models.TransientModel):
     _inherit = 'mail.compose.message'
@@ -193,73 +205,3 @@ class MailWizzardSignature(models.TransientModel):
         if self.env.user.get_email():
             return formataddr((self.env.user.get_name(), self.env.user.get_email()))
         raise UserError(_("Unable to send email, please configure the sender's email address."))
-
-    @api.multi
-    def get_mail_values(self, res_ids):
-        """Generate the values that will be used by send_mail to create mail_messages
-        or mail_mails. """
-        self.ensure_one()
-        results = dict.fromkeys(res_ids, False)
-        rendered_values = {}
-        mass_mail_mode = self.composition_mode == 'mass_mail'
-        # render all template-based value at once
-        if mass_mail_mode and self.model:
-            rendered_values = self.render_message(res_ids)
-        # compute alias-based reply-to in batch
-        reply_to_value = dict.fromkeys(res_ids, None)
-        if mass_mail_mode and not self.no_auto_thread:
-            # reply_to_value = self.env['mail.thread'].with_context(thread_model=self.model).browse(res_ids).message_get_reply_to(default=self.email_from)
-            reply_to_value = self.env['mail.thread'].with_context(thread_model=self.model).message_get_reply_to(res_ids,
-                                                                                                                default=self.email_from)
-        for res_id in res_ids:
-            # static wizard (mail.message) values
-            mail_values = {
-                'subject': self.subject,
-                'body': self.body or '',
-                'parent_id': self.parent_id and self.parent_id.id,
-                'partner_ids': [partner.id for partner in self.partner_ids],
-                'attachment_ids': [attach.id for attach in self.attachment_ids],
-                'author_id': self.author_id.id,
-                'email_from': self._get_default_from(),
-                'record_name': self.record_name,
-                'no_auto_thread': self.no_auto_thread,
-                'mail_server_id': self.mail_server_id.id,
-            }
-
-            # mass mailing: rendering override wizard static values
-            if mass_mail_mode and self.model:
-                if self.model in self.env and hasattr(self.env[self.model], 'message_get_email_values'):
-                    mail_values.update(self.env[self.model].browse(res_id).message_get_email_values())
-                # keep a copy unless specifically requested, reset record name (avoid browsing records)
-                mail_values.update(notification=not self.auto_delete_message, model=self.model, res_id=res_id,
-                                   record_name=False)
-                # auto deletion of mail_mail
-                if self.auto_delete or self.template_id.auto_delete:
-                    mail_values['auto_delete'] = True
-                # rendered values using template
-                email_dict = rendered_values[res_id]
-                mail_values['partner_ids'] += email_dict.pop('partner_ids', [])
-                mail_values.update(email_dict)
-                if not self.no_auto_thread:
-                    mail_values.pop('reply_to')
-                    if reply_to_value.get(res_id):
-                        mail_values['reply_to'] = reply_to_value[res_id]
-                if self.no_auto_thread and not mail_values.get('reply_to'):
-                    mail_values['reply_to'] = mail_values['email_from']
-                # mail_mail values: body -> body_html, partner_ids -> recipient_ids
-                mail_values['body_html'] = mail_values.get('body', '')
-                mail_values['recipient_ids'] = [(4, id) for id in mail_values.pop('partner_ids', [])]
-
-                # process attachments: should not be encoded before being processed by message_post / mail_mail create
-                mail_values['attachments'] = [(name, base64.b64decode(enc_cont)) for name, enc_cont in
-                                              email_dict.pop('attachments', list())]
-                attachment_ids = []
-                for attach_id in mail_values.pop('attachment_ids'):
-                    new_attach_id = self.env['ir.attachment'].browse(attach_id).copy(
-                        {'res_model': self._name, 'res_id': self.id})
-                    attachment_ids.append(new_attach_id.id)
-                mail_values['attachment_ids'] = self.env['mail.thread']._message_preprocess_attachments(
-                    mail_values.pop('attachments', []),
-                    attachment_ids, 'mail.message', 0)
-            results[res_id] = mail_values
-        return results
